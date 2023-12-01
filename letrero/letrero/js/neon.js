e6e97f6aed376ff6cabb5c5cc430e345495005d2
Esteban Lucas <estelukas@live.com>
2023-12-01 17:19:46 -0600

    Se quita fondo de letra apeticion de Diego

--- letrero/letrero/js/neon.js
+++ letrero/letrero/js/neon.js
@@ -5,8 +5,8 @@ function ConvertirNeon(){
     }
     var colorTextoNeon = document.getElementById("txtColor").value;
     document.getElementById("txtLetreroNeon").style.fontFamily =document.getElementById("dpdFuentes").value;
-    document.getElementById("txtLetreroNeon").style.color = colorTextoNeon
-    document.getElementById("txtLetreroNeon").style.textShadow ="0 0 25px rgba(0,0,0,1) , 0 0 5px rgba(0,0,0,1) , 0 0 2px rgba(0,0,0,1) , 0 0 15px "+ colorTextoNeon +" , 0 0 8px "+ colorTextoNeon+" , 0 0 8px "+ colorTextoNeon+" , 0 0 85px "+colorTextoNeon;
+    document.getElementById("txtLetreroNeon").style.color = "black"
+    document.getElementById("txtLetreroNeon").style.textShadow ="0 0 25px rgba(0,0,0,5) , 0 0 5px rgba(0,0,0,1) , 0 0 2px rgba(0,0,0,1) , 0 0 35px "+ colorTextoNeon +" , 0 0 35px "+ colorTextoNeon+" , 0 0 35px "+ colorTextoNeon+" , 0 0 35px "+colorTextoNeon;
     document.getElementById("txtLetreroNeon").innerHTML= document.getElementById("txtLetrero").value;
 
 }
